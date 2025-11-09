2e7ea980150ce185d31f19fd03504822e8bcfca2
wkpc <145634104+wkpc@users.noreply.github.com>
2025-11-09 10:57:31 -0700

    - added difficulty buttons and label - window name

--- src/main/java/minesweeper/minesweeper6/MainController.java
+++ src/main/java/minesweeper/minesweeper6/MainController.java
@@ -1,14 +1,55 @@
 package minesweeper.minesweeper6;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private Label InternalIssueNoti;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label UserActionNoti;
+
+    @FXML
+    private Label curDiff;
+
+    @FXML
+    private Button difficultyEasy;
+
+    @FXML
+    private Button difficultyHard;
+
+    @FXML
+    private Button difficultyNormal;
+
+    @FXML
+    private Button start;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
+    @FXML
+    void onDifficultyHardClicked(MouseEvent event)
+    {
+        curDiff.setText("Hard");
+    }
+
+    @FXML
+    void onDifficultyNormalClicked(MouseEvent event)
+    {
+        curDiff.setText("Normal");
+    }
+
+    @FXML
+    void onDifficultyEasyClicked(MouseEvent event)
+    {
+        curDiff.setText("Easy");
     }
 }
