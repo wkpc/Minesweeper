e60af70ec8ff3cd9fec4b348a501d90b92d97bc6
wkpc <145634104+wkpc@users.noreply.github.com>
2025-11-09 10:27:20 -0700

    Initial commit

---
+++ src/main/java/minesweeper/minesweeper6/MainController.java
@@ -0,0 +1,14 @@
+package minesweeper.minesweeper6;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
